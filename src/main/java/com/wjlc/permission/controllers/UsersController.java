7fa0d3cd5e148e754850576a5767a65e95d68820
lhnicker <lhnicker@hotmail.com>
2017-11-03 15:51:23 +0800

    修改UsersController的映射路径

--- src/main/java/com/wjlc/permission/controllers/UsersController.java
+++ src/main/java/com/wjlc/permission/controllers/UsersController.java
@@ -7,6 +7,7 @@ import com.wjlc.permission.dtoes.GetUserParam;
 import com.wjlc.permission.dtoes.UsersDto;
 
 @RestController
+@RequestMapping(value = "/permission")
 public class UsersController {
 	
 	@RequestMapping(value = "/userinfo")
