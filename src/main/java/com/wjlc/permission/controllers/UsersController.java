02597af0c2174c535f827cc989096a27c98fef7a
lhnicker <lhnicker@126.com>
2017-11-27 19:33:35 +0800

    update

--- src/main/java/com/wjlc/permission/controllers/UsersController.java
+++ src/main/java/com/wjlc/permission/controllers/UsersController.java
@@ -1,17 +1,16 @@
 package com.wjlc.permission.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.wjlc.common.BaseDto;
-import com.wjlc.permission.dto.GetUserParam;
 import com.wjlc.permission.dto.UsersDto;
+import com.wjlc.permission.dto.GetUserParam;
 
 @RestController
 @RequestMapping(value = "/permission")
 public class UsersController {
 	
-	@RequestMapping(value = "/userinfo")
+	@RequestMapping(value = "/userinfo", method = {RequestMethod.POST})
 	public BaseDto GetUserInfo(GetUserParam getUserParam) {
 		UsersDto usersDto = new UsersDto();
 		BaseDto retDto = new BaseDto();
